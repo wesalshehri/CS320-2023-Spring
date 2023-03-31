13b677ac200bf455c0a19567ad4f8c2086d03eba
wesalshehri <113001924+wesalshehri@users.noreply.github.com>
2023-03-31 18:58:40 -0400

    Update assign06_04.py

--- assigns/06/MySolution/Python/assign06_04.py
+++ assigns/06/MySolution/Python/assign06_04.py
@@ -4,33 +4,78 @@
 import sys
 sys.path.append('./../../../../mypylib')
 from mypylib_cls import *
+import queue
+import 
 ####################################################
 """
 HX-2023-03-24: 30 points
 Solving the doublet puzzle
 """
 ####################################################
+# assign05_02
+
+def string_length(word):
+    return len(word)
+
+def strsub(word, i):
+    return word[i]
+
+
+def AB():
+    return "abcdefghijklmnopqrstuvwxyz"
+
+def string_implode(s):
+    return fnlist_foldleft(s, "", lambda acc, x: acc + x)
+
+
+def fnlist_tabulate(n, fopr):
+    if n <= 0:
+        return fnlist_nil()
+    else:
+        return fnlist_cons(fopr(0), fnlist_tabulate(n - 1, lambda i: fopr(i + 1)))
+
+def string_tabulate(l, fopr):
+   return string_implode(fnlist_tabulate(l, fopr))
+
+
+def word_neighbors(word):
+    """                                                                                                                                                       
+    Note that this function should returns a fnlist                                                                                                           
+    (not a pylist)                                                                                                                                            
+    Your implementation should be combinator-based very                                                                                                       
+    much like the posted solution.                                                                                                                            
+    """
+    wlen = string_length(word)
+    return \
+        fnlist_concat(string_imap_fnlist\
+                      (word, lambda i, c: fnlist_concat\
+                       (string_imap_fnlist(AB(), lambda _, c1: fnlist_sing\
+                                           (string_tabulate(wlen, lambda j: strsub(word, j) if i != j else c1)) if (c != c1) else fnlist_nil()))))
+            
+
+def word_neighbors_legal(word):
+    return fnlist_filter_pylist(word_neighbors(word), word_is_legal)
+
+def wpath_neighbors_legal(wpath):
+    word1 = wpath[-1]
+    words = word_neighbors_legal(word1)
+    return [wpath + (word2,) for word2 in words]
+
+def gtree_bfs(nxs, fchildren):
+    def helper(nxs):
+        if nxs.empty():
+            return strcon_nil()
+        else:
+            nx1 = nxs.get()
+            for nx2 in fchildren(nx1):
+                nxs.put(nx2)
+            return strcon_cons(nx1, gtree_bfs(nxs, fchildren))
+    return lambda: helper(nxs)
+
 def doublet_stream_from(word):
-"""
-Please revisit assign05_02.py.
-######
-Given a word w1 and another word w2, w1 and w2 are a
-1-step doublet if w1 and w2 differ at exactly one position.
-For instance, 'water' and 'later' are a 1-step doublet.
-The doublet relation is the reflexive and transitive closure
-of the 1-step doublet relation. In other words, w1 and w2 are
-a doublet if w1 and w2 are the first and last of a sequence of
-words where every two consecutive words form a 1-step doublet.
-Here is a little website where you can use to check if two words
-for a doublet or not:
-http://ats-lang.github.io/EXAMPLE/BUCS320/Doublets/Doublets.html
-######
-Given a word, the function [doublet_stream_from] returns a stream
-enumerating *all* the tuples such that the first element of the tuple
-is the given word and every two consecutive words in the tuple form a
-1-step doublet. The enumeration of tuples should be done so that shorter
-tuples are always enumerated ahead of longer ones.
-######
-"""
-    raise NotImplementedError
+    nxs = queue.Queue() 
+    nxs.put((word,))
+    return gtree_bfs(nxs, wpath_neighbors_legal)
+
+
 ####################################################
