dbfe98fe0ada359aa111ac784a248489fd85e84a
wesalshehri <113001924+wesalshehri@users.noreply.github.com>
2023-04-07 16:19:27 -0400

    Update assign07_03.py

--- assigns/07/MySolution/Python/assign07_03.py
+++ assigns/07/MySolution/Python/assign07_03.py
@@ -31,6 +31,49 @@ http://ats-lang.github.io/EXAMPLE/BUCS320/Doublets/Doublets.html
 ######
 """
 ####################################################
+def string_length(word):
+    return len(word)
+
+def strsub(word, i):
+    return word[i]
+
+
+def AB():
+    return "abcdefghijklmnopqrstuvwxyz"
+
+def string_implode(s):
+    return fnlist_foldleft(s, "", lambda acc, x: acc + x)
+
+
+def fnlist_tabulate(n, fopr):
+    if n <= 0:
+        return fnlist_nil()
+    else:
+        return fnlist_cons(fopr(0), fnlist_tabulate(n - 1, lambda i: fopr(i + 1)))
+
+def string_tabulate(l, fopr):
+   return string_implode(fnlist_tabulate(l, fopr))
+
+
+def word_neighbors(word):
+    """                                                                                                                                                       
+    Note that this function should returns a fnlist                                                                                                           
+    (not a pylist)                                                                                                                                            
+    Your implementation should be combinator-based very                                                                                                       
+    much like the posted solution.                                                                                                                            
+    """
+    wlen = string_length(word)
+    return \
+        fnlist_concat(string_imap_fnlist\
+                      (word, lambda i, c: fnlist_concat\
+                       (string_imap_fnlist(AB(), lambda _, c1: fnlist_sing\
+                                           (string_tabulate(wlen, lambda j: strsub(word, j) if i != j else c1)) if (c != c1) else fnlist_nil()))))
+            
+            
+            
+def helper(x):
+    return foreach_to_filter_fnlist(fnlist_foreach)(word_neighbors(x), lambda word: word_is_legal(word))
+
 def doublet_bfs_test(w1, w2):
     """
     Given two words w1 and w2, this function should
@@ -38,5 +81,10 @@ def doublet_bfs_test(w1, w2):
     it returns a path connecting w1 and w2 that attests to the
     two words forming a doublet.
     """
-    raise NotImplementedError
+    temp = stream_make_filter(gpath_bfs([w1], helper), lambda path: path[-1] == w2)()
+    if temp.get_ctag() == 0:
+        return None
+    else:
+        return temp.get_cons1()
+    
 ####################################################
